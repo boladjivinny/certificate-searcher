74a424605d007318e25538b182e992caf62a30ed
zanema <amenaz1@gmail.com>
2020-05-19 15:48:00 -0500

    json parsing of malcerts.json

--- ruby/whois-fetcher.rb
+++ ruby/whois-fetcher.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'logger'
+require 'oj'
 require 'optparse'
 require 'parallel'
 require 'set'
@@ -12,7 +13,7 @@ logger = Logger.new(STDOUT)
 params = {}
 OptionParser.new do |opts|
 	opts.banner = "Usage whois.rb [options] "
-	opts.on("-i", "--input INPUTFILE", "INPUTFILE with domains to look up")
+	opts.on("-i", "--input INPUTFILE", "INPUTFILE with domains to look up (can be malcerts json or domain txt)")
   opts.on("-o", "--output OUTPUT_DIR", "OUTPUT_DIR for storing whois records")
 	puts opts if ARGV.length == 0 
 end.parse!(into: params)
@@ -20,16 +21,36 @@ end.parse!(into: params)
 whois_hosts_domains = {}
 unknown_whois_host_domains = Set.new
 
-File.readlines(params[:input]).each do |line|
-	domain = line.rstrip.split(",")[0]
-  host = Whois::Server.find_for_domain(domain)&.host
+case File.extname(params[:input])
+when ".txt"
+  File.readlines(params[:input]).each do |line|
+    domain = line.rstrip.split(",")[0]
+    host = Whois::Server.find_for_domain(domain)&.host
 
-  if host.nil?
-    unknown_whois_host_domains.add(domain)
-  else
-    whois_hosts_domains[host] = [] unless whois_hosts_domains.key?(host)
-    whois_hosts_domains[host] << domain
+    if host.nil?
+      unknown_whois_host_domains.add(domain)
+    else
+      whois_hosts_domains[host] = [] unless whois_hosts_domains.key?(host)
+      whois_hosts_domains[host] << domain
+    end
   end
+when ".json"
+  File.readlines(params[:input]).each do |line|
+    obj = Oj.load(line.rstrip)
+    obj['abuse_domains'].each do |maldomain, maltype|
+      host = Whois::Server.find_for_domain(maldomain)&.host
+
+      if host.nil?
+        unknown_whois_host_domains.add(maldomain)
+      else
+        whois_hosts_domains[host] = [] unless whois_hosts_domains.key?(host)
+        whois_hosts_domains[host] << maldomain
+      end
+    end
+  end
+else
+  puts "Unsupported filetype: #{params[:input]}. Only json/txt are supported."
+  exit(1)
 end
 
 # sort whois_hosts to prioritize shorter domains (less likely to be a long chained dumb domain)
