51bab3c79f6b2d557ca9bb222b9e0ef2f1736f6a
zanema <amenaz1@gmail.com>
2020-05-19 18:26:53 -0500

    oops - add back actual whois querying

--- ruby/whois-fetcher.rb
+++ ruby/whois-fetcher.rb
@@ -58,21 +58,21 @@ else
   exit(1)
 end
 
-# sort whois_hosts to prioritize shorter domains (less likely to be a long chained dumb domain)
-# whois_hosts_domains.each do |whois_host, domains|
-#   whois_hosts_domains[whois_host] = domains.sort_by(&:length)
-# end
-#
-# whois_hosts = whois_hosts_domains.keys
-# Parallel.map(whois_hosts, in_threads: whois_hosts.length) do |whois_host|
-#   whois_hosts_domains[whois_host].each do |domain|
-#     output_filename = File.join(params[:output], domain)
-#     next if File.exists?(output_filename)
-#
-#     logger.info("Looking up WHOIS for #{domain} from #{whois_host}")
-#     record = Whois.whois(domain)
-#     serialized = YAML::dump(record)
-#     File.write(output_filename, serialized)
-#     sleep(10)
-#   end
-# end
+sort whois_hosts to prioritize shorter domains (less likely to be a long chained dumb domain)
+whois_hosts_domains.each do |whois_host, domains|
+  whois_hosts_domains[whois_host] = domains.sort_by(&:length)
+end
+
+whois_hosts = whois_hosts_domains.keys
+Parallel.map(whois_hosts, in_threads: whois_hosts.length) do |whois_host|
+  whois_hosts_domains[whois_host].each do |domain|
+    output_filename = File.join(params[:output], domain)
+    next if File.exists?(output_filename)
+
+    logger.info("Looking up WHOIS for #{domain} from #{whois_host}")
+    record = Whois.whois(domain)
+    serialized = YAML::dump(record)
+    File.write(output_filename, serialized)
+    sleep(10)
+  end
+end
