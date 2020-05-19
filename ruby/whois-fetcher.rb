e0c9594ef31a4cff1a919ea7f346b54120667e7f
zanema <amenaz1@gmail.com>
2020-05-19 18:25:40 -0500

    skip over phishtank and gsb for whois

--- ruby/whois-fetcher.rb
+++ ruby/whois-fetcher.rb
@@ -38,7 +38,10 @@ when ".txt"
 when ".json"
   File.readlines(params[:input]).each do |line|
     obj = Oj.load(line.rstrip)
-    obj['abuse_domains'].each do |maldomain, maltype|
+    obj['abuse_domains'].each do |maldomain, maltypes|
+      # skip over google safe browsing / phishtank
+      next unless (maltypes.keys & ["GOOGLE_SAFEBROWSING", "PHISHTANK"]).empty?
+
       maldomain = maldomain[2..-1] if maldomain.start_with?("*.")
       host = Whois::Server.find_for_domain(maldomain)&.host
 
@@ -56,20 +59,20 @@ else
 end
 
 # sort whois_hosts to prioritize shorter domains (less likely to be a long chained dumb domain)
-whois_hosts_domains.each do |whois_host, domains|
-  whois_hosts_domains[whois_host] = domains.sort_by(&:length)
-end
-
-whois_hosts = whois_hosts_domains.keys
-Parallel.map(whois_hosts, in_threads: whois_hosts.length) do |whois_host|
-  whois_hosts_domains[whois_host].each do |domain|
-    output_filename = File.join(params[:output], domain)
-    next if File.exists?(output_filename)
-
-    logger.info("Looking up WHOIS for #{domain} from #{whois_host}")
-    record = Whois.whois(domain)
-    serialized = YAML::dump(record)
-    File.write(output_filename, serialized)
-    sleep(10)
-  end
-end
+# whois_hosts_domains.each do |whois_host, domains|
+#   whois_hosts_domains[whois_host] = domains.sort_by(&:length)
+# end
+#
+# whois_hosts = whois_hosts_domains.keys
+# Parallel.map(whois_hosts, in_threads: whois_hosts.length) do |whois_host|
+#   whois_hosts_domains[whois_host].each do |domain|
+#     output_filename = File.join(params[:output], domain)
+#     next if File.exists?(output_filename)
+#
+#     logger.info("Looking up WHOIS for #{domain} from #{whois_host}")
+#     record = Whois.whois(domain)
+#     serialized = YAML::dump(record)
+#     File.write(output_filename, serialized)
+#     sleep(10)
+#   end
+# end
