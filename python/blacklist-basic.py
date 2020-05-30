e92830d1434ffb2bddcf2378da37dd68d94fbdf8
zanema <amenaz1@gmail.com>
2020-05-30 11:07:43 -0500

    blacklist protocol transitions

--- python/blacklist-basic.py
+++ python/blacklist-basic.py
@@ -48,9 +48,19 @@ class DomainHistory:
 protocol_etldPlusOnes = {}
 protocol_domains = {}
 
+previous_http_e2ld = set()
+previous_https_e2ld = set()
+
 for fpath in sorted(glob.glob(os.path.join(blacklist_dir, "*.csv")))[0::7]:
     logger.info(f"reading {fpath}")
     date = "-".join(fpath.split('/')[-1].split('-')[:3])
+
+    new_https = set()
+    still_https = set()
+    http_to_https = set()
+    new_http = set()
+    still_http = set()
+    https_to_http = set()
 
     gsb_paths = glob.glob(os.path.join(safebrowsing_dir, date + "/", f"{date}T00*-results.txt"))
     if len(gsb_paths) == 0:
@@ -73,11 +83,27 @@ for fpath in sorted(glob.glob(os.path.join(blacklist_dir, "*.csv")))[0::7]:
             eTLD_plus_one = row['ETLDPlus1']
             mal_category = row['Category']
 
-            if mal_category != 'Phishing':
+            if mal_category not in ['Phishing', 'Fraud']:
                 continue
 
             protocol_etldPlusOnes[date][protocol].add(eTLD_plus_one)
             protocol_domains[date][protocol].add(domain)
+
+            if protocol == "https":
+                if eTLD_plus_one in previous_https_e2ld:
+                    still_https.add(eTLD_plus_one)
+                elif eTLD_plus_one in previous_http_e2ld:
+                    http_to_https.add(eTLD_plus_one)
+                else:
+                    new_https.add(eTLD_plus_one)
+
+            if protocol == "http":
+                if eTLD_plus_one in previous_http_e2ld:
+                    still_http.add(eTLD_plus_one)
+                elif eTLD_plus_one in previous_https_e2ld:
+                    https_to_http.add(eTLD_plus_one)
+                else:
+                    new_http.add(eTLD_plus_one)
 
     logger.info(f"reading {gsb_path}")
     with open(gsb_path) as gsb:
@@ -95,12 +121,30 @@ for fpath in sorted(glob.glob(os.path.join(blacklist_dir, "*.csv")))[0::7]:
             ext = tldextract.extract(entry)
             domain = '.'.join(ext).strip('.')
             eTLD_plus_one = '.'.join(ext[1:]).strip('.')
-            print(domain)
 
             protocol = entry.split(':')[0]
 
             protocol_domains[date][protocol].add(domain)
             protocol_etldPlusOnes[date][protocol].add(eTLD_plus_one)
+
+            if protocol == "https":
+                if eTLD_plus_one in previous_https_e2ld:
+                    still_https.add(eTLD_plus_one)
+                elif eTLD_plus_one in previous_http_e2ld:
+                    http_to_https.add(eTLD_plus_one)
+                else:
+                    new_https.add(eTLD_plus_one)
+
+            if protocol == "http":
+                if eTLD_plus_one in previous_http_e2ld:
+                    still_http.add(eTLD_plus_one)
+                elif eTLD_plus_one in previous_https_e2ld:
+                    https_to_http.add(eTLD_plus_one)
+                else:
+                    new_http.add(eTLD_plus_one)
+
+    previous_http_e2ld = still_http | https_to_http | new_http
+    previous_https_e2ld = still_https | http_to_https | new_https
 
     with open(output_file, "a") as w:
         writer = csv.writer(w, quoting=csv.QUOTE_MINIMAL)
@@ -111,9 +155,20 @@ for fpath in sorted(glob.glob(os.path.join(blacklist_dir, "*.csv")))[0::7]:
         http_domains = len(protocol_domains[date]["http"])
         https_domains = len(protocol_domains[date]["https"])
         percent_https_domains = https_domains / (https_domains + http_domains)
-
-        writer.writerow(
-            [date, http_domains, https_domains, percent_https_domains, http_etldPlusOnes, https_etldPlusOnes,
-             percent_https_etldPlusOnes])
+        writer.writerow([
+            date,
+            http_domains,
+            https_domains,
+            percent_https_domains,
+            http_etldPlusOnes,
+            https_etldPlusOnes,
+            percent_https_etldPlusOnes,
+            len(new_http),
+            len(https_to_http),
+            len(still_http),
+            len(new_https),
+            len(http_to_https),
+            len(still_https),
+        ])
 
         w.flush()
